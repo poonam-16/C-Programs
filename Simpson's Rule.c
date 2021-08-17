84ce6f08497a0595d8fa42479bd53ea1bc17e1b5
Poonam <poonamdeshmukh616@gmail.com>
2021-08-17 19:30:16 +0530

    Simpson's 1/3 rd Rule

---
+++ Simpson's Rule.c
@@ -0,0 +1,34 @@
+//Solve integral 2 to 3 of (2x+3) dx using simpsons 1/3 rule
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define f(x)  exp(x) - 7
+main()
+{
+	float a,b,fa,fb,f,h,fe,fo,sum = 0,sum1 = 0,sum2 = 0,total;
+	int i,n;
+	printf("Enter no. of sub-intervals : ");
+	scanf("%d",&n);
+	printf("\nEnter lower limit : ");
+	scanf("%f",&a);
+	printf("\nEnter upper limit : ");
+	scanf("%f",&b);
+	h = (b-a)/n;
+	//fa = (2*a) + 3;
+	//fb = (2*b) + 3;
+	sum = f(a) + f(b);
+	for(i=1;i<n;i=i+2) //odd terms
+	{
+		//fo = 2*(a+(i*h)) + 3;
+		sum1 = sum1 + f(a+i*h);
+	}
+	for(i=2;i<n;i=i+2) //even terms
+	{
+		//fe = 2*(a+(i*h)) + 3;
+		sum2 = sum2 + f(a+i*h);
+	}
+	sum = sum + 4*sum1 + 2*sum2;
+	total = (h/3)*sum;
+	printf("\nThe value of integral is: %0.2f",total);
+	return 0;
+}

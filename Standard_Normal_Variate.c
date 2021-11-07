566c7e597a316b6d2f1f1dae2639ffb3e069ef46
Poonam <poonamdeshmukh616@gmail.com>
2021-11-07 23:47:47 +0530

    Add files via upload

---
+++ Standard_Normal_Variate.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define f(x)  exp(-pow(x,2)/2)*(1/sqrt(2*3.14))
+main()
+{
+	float a,b,fa,fb,f,h,sum = 0,sum1 = 0,sum2 = 0,total;
+	int i,n;
+	printf("Enter no. of sub-intervals : ");
+	scanf("%d",&n);
+	printf("\nEnter lower limit : ");
+	scanf("%f",&a);
+	printf("\nEnter upper limit : ");
+	scanf("%f",&b);
+	h = (b-a)/n;
+	sum = f(a) + f(b);
+	for(i=1;i<n;i=i+2) //odd terms
+	{
+		sum1 = sum1 + f(a+i*h);
+	}
+	for(i=2;i<n;i=i+2) //even terms
+	{
+		sum2 = sum2 + f(a+i*h);
+	}
+	sum = sum + 4*sum1 + 2*sum2;
+	total = (h/3)*sum;
+	printf("\nThe value of integral is: %0.4f",total);
+	return 0;
+}
